8191ed0545b784f6b77427c4a8a61f983ff78392
Rain <dudu.silva7@gmail.com>
2021-06-05 17:07:34 -0300

    rede

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -6,7 +6,6 @@ exports.Insert = (req, res, next) => {
     const descricao = req.body.descricao;
     const preco = req.body.preco;
     const quantemEstoque = req.body.quantemEstoque;
-    const ativo = req.body.ativo;
  
     Produtos.create({
         nome: nome,
