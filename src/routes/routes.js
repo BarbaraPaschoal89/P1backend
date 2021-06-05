d4675241b49016f607d1606ef0b67f7e8dc2de72
Rain <dudu.silva7@gmail.com>
2021-06-05 10:43:03 -0300

    insert

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,7 +5,6 @@ const router = express.Router();
 router.post('/produtos', ProdutosController.Insert);
 router.get('/produtos', ProdutosController.SelectAll);
 router.get('/produtos/:id', ProdutosController.SelectDetail);
-router.put('/produtos/:id', ClienteController.Update);
-router.delete('/produtos/:id', ClienteController.Delete);
- 
+router.put('/produtos/:id', ProdutosController.Update);
+router.delete('/produtos/:id', ProdutosController.Delete); 
 module.exports = router;
