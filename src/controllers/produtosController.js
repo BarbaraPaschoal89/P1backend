3eefc903535a1022b1e8c1b7137c13c96e489851
Rain <dudu.silva7@gmail.com>
2021-06-05 12:21:13 -0300

    update and delente

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -46,3 +46,56 @@ exports.SelectDetail = (req, res, next) => {
         })
         .catch(error => next(error));
 };
+
+exports.Update = (req, res, next) => {
+    const id = req.params.id;
+    const nome = req.body.nome;
+    const descricao = req.body.descricao;
+    const preco = req.body.preco;
+    const quantemEstoque = req.body.quantemEstoque;
+    const ativo = req.body.ativo;
+
+    Produtos.findByPk(id)
+        .then(produtos => {
+            if (produtos) {
+                produtos.update({
+                    nome: nome,
+                    descricao: descricao,
+                    preco: preco,
+                    quantemEstoque: quantemEstoque,
+                    ativo: ativo
+                },
+                    {
+                        where: { id:id }
+                    })
+                .then(() => {
+                    res.status(status.OK).send();
+                })
+                .catch(errir => next (error));
+            } else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error =>(error));
+};
+
+exports.Delete = (req, res, next) => {
+    const id = req.params.id;
+ 
+    Produtos.findByPk(id)
+        .then(produtos => {
+            if (produtos) {
+                produtos.destroy({
+                    where: { id: id }
+                })
+                    .then(() => {
+                        res.status(status.OK).send();
+                    })
+                    .catch(error => next(error));
+            }
+            else {
+                res.status(status.NOT_FOUND).send();
+            }
+        })
+        .catch(error => next(error));
+};
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -7,4 +7,5 @@ router.get('/produtos', ProdutosController.SelectAll);
 router.get('/produtos/:id', ProdutosController.SelectDetail);
 router.put('/produtos/:id', ProdutosController.Update);
 router.delete('/produtos/:id', ProdutosController.Delete); 
+
 module.exports = router;
